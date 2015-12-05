16150abc2b6980908a82bb0ef0a9686d7fba46ba
raddle60 <raddle60@users.noreply.github.com>
2015-12-05 22:48:06 +0800

    启动main方法

--- src/test/java/com/socket/agent/SocketMiddleTest.java
+++ src/test/java/com/socket/agent/SocketMiddleTest.java
@@ -1,7 +1,5 @@
 package com.socket.agent;
 
-import com.socket.agent.middle.SocketMiddleClient;
-import com.socket.agent.model.SocketMiddleFoward;
 
 /**
  * description: 
@@ -15,12 +13,7 @@ public class SocketMiddleTest {
      * @throws InterruptedException 
      */
     public static void main(String[] args) throws InterruptedException {
-//        SocketMiddleServer middleServer = new SocketMiddleServer();
-//        middleServer.getMiddles().add(new SocketMiddle(10004, 10002));
-//        middleServer.start();
-        SocketMiddleClient client = new SocketMiddleClient();
-        client.getMiddles().add(new SocketMiddleFoward("127.0.0.1:8082", "127.0.0.1:8080"));
-        client.start();
+        SocketAgentMain.main(args);
     }
 
 }
